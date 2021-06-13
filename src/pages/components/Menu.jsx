070dc2b610304d6ecfad92e1486ba080548924e0
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-06-12 20:37:16 -0500

    componentes para las paginas

---
+++ src/pages/components/Menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MenuIcon from './svg/MenuIcon'
+
+export const Menu = () => {
+	return (
+		<div class='menu__icon' id='menu-icon'>
+			<MenuIcon />
+		</div>
+	)
+}
